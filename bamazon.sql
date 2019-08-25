55b403203f1a2493288b061ee88f4020c8130fec
Zeus <reeseborchersmsu@gmail.com>
2019-08-24 23:44:00 -0500

    sql database

---
+++ bamazon.sql
@@ -0,0 +1,42 @@
+CREATE DATABASE bamazon_db;
+
+USE bamazon_db;
+
+CREATE TABLE products (
+id INT NOT NULL AUTO_INCREMENT,
+product_name VARCHAR(30) NULL,
+department_name VARCHAR(30) NULL,
+price DECIMAL (10,2) NULL,
+stock_quantity 	INT NULL,
+PRIMARY KEY (id)
+);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('Xbox One', 'electronics', 259.99, 60);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('MacBook Pro', 'electronics', 2899.99, 22);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('Burton Snowboard', 'sporting goods', 2899.99, 14);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('TWP Dogfood', 'pets', 49.99, 346);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('Diapers', 'baby', 39.99, 782);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('Dog Bed', 'pets', 59.99, 925);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('The Giver', 'books', 39.99, 67);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('Shampoo', 'beauty', 9.99, 2249);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('Alpha Bounce', 'shoes', 99.99, 81);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('Yogurt', 'groceries', 2.99, 163);
